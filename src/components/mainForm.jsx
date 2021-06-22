f4769400e6ce227e7645af640f7a2c88a4df8c02
AndriusDulevicius <andriusdulevicius@email.com>
2021-06-22 08:59:19 +0300

    main form cc , plus some basic style

--- src/components/mainForm.jsx
+++ src/components/mainForm.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
 class MainForm extends Component {
-  state = {  }
-  render() { 
-    return (  );
+  state = {};
+  render() {
+    return (
+      <div className='main-form'>
+        <h1>Main form</h1>
+      </div>
+    );
   }
 }
- 
+
 export default MainForm;
